4aabd8d1ad7da4e07f01ce5473f573858b5544b1
Josh.ep <90182193+joshchan1301@users.noreply.github.com>
2026-02-28 15:14:42 +0700

    Update models file

--- models.py
+++ models.py
@@ -1,3 +1,27 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    email = Column(String)
+
+    # Quan hệ: Một User thì có nhiều Posts
+    posts = relationship("Post", back_populates="owner")
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    # Khóa ngoại liên kết với users.
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    # Quan hệ ngược lại: Post này thuộc về ai ?
+    owner = relationship("User", back_populates="posts")
