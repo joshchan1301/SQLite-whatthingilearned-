891d0ea927153b789d6c03d4113abd1050c8f218
Josh.ep <90182193+joshchan1301@users.noreply.github.com>
2026-02-28 16:39:10 +0700

    update fix lỗi sai chính tả autocommit và users

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ DATABASE_URL = "sqlite:///./blog_app.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 # SessionLocal: Phiên làm việc (Giống như một phiên giao dịch tại ngân hàng)
-SessionLocal = sessionmaker(autocomit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Base: lớp cơ sở để các Models khác kế thừa
 Base = declarative_base()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from database import SessionLocal, engine, Base
+from database import engine, Base, SessionLocal
 from models import User, Post
 
 # 1. Tạo các bảng trong Database (nếu chưa có)
@@ -9,17 +9,27 @@ db = SessionLocal()
 
 try:
     # 3. Tạo một User mới
-    new_user = User(username="fresher_dev", email="dev@example.com")
-    db.add(new_user)
-    db.commit()  # Lưu User để lấy được ID
-    db.refresh(new_user)  # Lấy lại thông tin User kèm theo ID vừa tạo
+    # new_user = User(username="fresher_dev", email="dev@example.com")
+    # db.add(new_user)
+    # db.commit()  # Lưu User để lấy được ID
+    # db.refresh(new_user)  # Lấy lại thông tin User kèm theo ID vừa tạo
 
-    # 4. Tạo bài viết cho User đó
-    new_post = Post(title="Học SQLAlchemy",
-                    content="Rất thú vị !", owner_id=new_user.id)
-    db.add(new_post)
-    db.commit()
+    # # 4. Tạo bài viết cho User đó
+    # new_post = Post(title="Học SQLAlchemy",
+    #                 content="Rất thú vị !", owner_id=new_user.id)
+    # db.add(new_post)
+    # db.commit()
 
-    print(f"Đã tạo User: {new_user.username} với bài viết: {new_post.title}")
+    # print(f"Đã tạo User: {new_user.username} với bài viết: {new_post.title}")
+
+    # Truy vấn tìm user có tên là "fresher_dev"
+    target_user = db.query(User).filter(User.username == "fresher_dev").first()
+    if target_user:
+        print(f"--Blog của {target_user.username}--")
+        # Nhờ có "relationship",ta có thể truy cập danh sách posts cực kỳ dễ dàng
+        for post in target_user.posts:
+            print(f"Tiêu đề: {post.title}")
+    else:
+        print("Không tìm thấy user này!")
 finally:
     db.close()  # Cực kỳ quan trọng: Luôn đóng session để giải phóng tài nguyên!
